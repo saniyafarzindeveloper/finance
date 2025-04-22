7506bfeac90560df6239a47e2fb62613cad16763
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-04-22 21:50:32 +0530

    added routes

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,11 +1,25 @@
-'use client'
+"use client";
 
 const routes = [
-    
-]
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+  },
+  { href: "/accounts", label: "Accounts" },
+  {
+    href: "/categories",
+    label: "Categories",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  }
+];
 
 export default function Navigation() {
-  return (
-    <div>Navigation</div>
-  )
+  return <div>Navigation</div>;
 }
